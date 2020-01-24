621ed8db1c8e4e5673580a8dc8a587bac36732c4
lu4ezar <misha-shisha@mail.ru>
2020-01-24 23:11:51 +0700

    + NotFound

---
+++ src/components/notFound/NotFound.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+const ErrorPage = (props: RouteComponentProps) => <main>Not Found</main>;
+
+export default ErrorPage;
