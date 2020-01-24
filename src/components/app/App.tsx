7a6351ccb326ed3ee41b6adc589545a46160d0dc
lu4ezar <misha-shisha@mail.ru>
2020-01-24 23:13:43 +0700

    modified App

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
+import Header from '../header';
+import Main from '../main';
+import Footer from '../footer';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 };
 
