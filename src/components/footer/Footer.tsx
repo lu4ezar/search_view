4674786b1f9ca72da8680d71228901f9384ad09d
lu4ezar <misha-shisha@mail.ru>
2020-01-25 15:41:44 +0700

    + Footer, Footer.scss

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import './Footer.scss';
 
-const Footer = () => <footer>Footer</footer>;
+const Footer = () => (
+  <footer>
+    <a
+      title="open project's github page in new tab"
+      href="https://github.com/lu4ezar/search_view"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      https://github.com/lu4ezar
+    </a>
+  </footer>
+);
 
 export default Footer;
