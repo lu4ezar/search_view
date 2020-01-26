f0f26d9a9331bb50d3013a90ff613e08c1320045
lu4ezar <misha-shisha@mail.ru>
2020-01-26 17:49:20 +0700

    + TestImages

---
+++ src/components/gallery/testImages/TestImages.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { images } from './testImages';
+import './TestImages.scss';
+
+interface TestImages {
+  onClick: (value: string) => void;
+  onSubmit: (value: string) => void;
+}
+
+const TestImages = ({ onClick, onSubmit }: TestImages) => {
+  const buttons = images.map((imgLink, index) => (
+    <button
+      key={index}
+      className="test-img-btn"
+      onClick={() => onClick(imgLink)}
+      onDoubleClick={() => onSubmit(imgLink)}
+      title="click to set image link, double click to auto-submit"
+    >{`#${index + 1}`}</button>
+  ));
+  return <div className="test-images">{buttons}</div>;
+};
+
+export default TestImages;
---
+++ src/components/gallery/testImages/testImages.ts
@@ -0,0 +1,13 @@
+export const images = [
+  'https://images.unsplash.com/photo-1565234329639-27fd21c04edb',
+  'https://images.unsplash.com/photo-1554171275-74ed5cb916cc',
+  'https://images.unsplash.com/photo-1575367725486-4183db79e018',
+  'https://images.unsplash.com/photo-1543637942-8127d74e9d3d',
+  'https://images.unsplash.com/photo-1516908205727-40afad9449a8',
+  'https://images.unsplash.com/photo-1518365658347-c4906efc5636',
+  'https://images.unsplash.com/photo-1540155993474-052b670771fc',
+  'https://images.unsplash.com/photo-1507150080056-79542bdb9c59',
+  'https://images.unsplash.com/photo-1421977870504-378093748ae6',
+  'https://images.unsplash.com/photo-1455889267303-a5ec970d6d43',
+  'https://images.unsplash.com/photo-1522871631682-a54fc957080e'
+];
