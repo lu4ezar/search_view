6d1c227fe4e8f49860f4dd8ba118625cc4022589
lu4ezar <misha-shisha@mail.ru>
2020-01-26 21:10:08 +0700

    updated NotFound

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import { RouteComponentProps, Link } from '@reach/router';
+import './NotFound.scss';
 
-const ErrorPage = (props: RouteComponentProps) => <>Not Found</>;
+const ErrorPage = (props: RouteComponentProps) => (
+  <div className="not-found">
+    <div>Not Found</div>
+    <div className="home-link">
+      <Link to="/">Back to Homepage</Link>
+    </div>
+  </div>
+);
 
 export default ErrorPage;
