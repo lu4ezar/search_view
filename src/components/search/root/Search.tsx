dfbb7f628fa54949e3a0a0bef518bf36450f160a
lu4ezar <misha-shisha@mail.ru>
2020-01-28 13:04:22 +0700

    Search: added fetchData function

--- src/components/search/root/Search.tsx
+++ src/components/search/root/Search.tsx
@@ -1,12 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import Input from '../../input';
 import Card from '../card';
-import useFetch from '../utils/useFetch';
+import { getQuery } from '../utils/query';
 
 const Search = (props: RouteComponentProps) => {
   const [id, setId] = useState('');
-  const [response, doFetch] = useFetch(id);
+  const [{ isLoading, data, error }, setFetchState] = useState({
+    isLoading: false,
+    data: {},
+    error: null
+  });
+
+  const fetchData = useCallback(async () => {
+    setFetchState({ isLoading: true, data: {}, error: null });
+    try {
+      const url = 'https://rickandmortyapi.com/graphql';
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: getQuery(id)
+        })
+      };
+      let response = await fetch(url, options);
+      const responseJSON: any = await response.json();
+      if (responseJSON.errors) {
+        throw new Error('GraphQL Error');
+      }
+      setFetchState({
+        isLoading: false,
+        data: responseJSON.data,
+        error: null
+      });
+    } catch (err) {
+      throw new Error('oi');
+    }
+    return [
+      {
+        isLoading,
+        data,
+        error
+      },
+      setId
+    ];
+  }, [data, error, id, isLoading]);
+
+  const response = {
+    isLoading,
+    data,
+    error
+  };
+
   return (
     <div className="container">
       <div className="main-header">Search</div>
@@ -15,9 +60,9 @@ const Search = (props: RouteComponentProps) => {
         value={id}
         type="text"
         onChange={setId}
-        onSubmit={doFetch}
+        onSubmit={fetchData}
       />
-      {id && <Card {...response} />}
+      <Card {...response} />
     </div>
   );
 };
