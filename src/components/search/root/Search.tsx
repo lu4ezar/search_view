7c3f95ef7cae8ea3730726bb8a0c45fa0f9e1e44
lu4ezar <misha-shisha@mail.ru>
2020-01-28 15:49:29 +0700

    fixed typing mismatches

--- src/components/search/root/Search.tsx
+++ src/components/search/root/Search.tsx
@@ -3,17 +3,34 @@ import { RouteComponentProps } from '@reach/router';
 import Input from '../../input';
 import Card from '../card';
 import { getQuery } from '../utils/query';
+import { Query } from '../../../types';
+
+type Errors = {
+  errors: Error;
+};
+
+type Data = {
+  data: Query;
+};
+
+type GraphQLResponse = Data | Errors;
+
+export type FetchData = {
+  isLoading: boolean;
+  data: Query | null;
+  error: string | null;
+};
 
 const Search = (props: RouteComponentProps) => {
   const [id, setId] = useState('');
   const [{ isLoading, data, error }, setFetchState] = useState({
     isLoading: false,
-    data: {},
+    data: null as FetchData['data'],
     error: null
   });
 
   const fetchData = useCallback(async () => {
-    setFetchState({ isLoading: true, data: {}, error: null });
+    setFetchState({ isLoading: true, data: null, error: null });
     try {
       const url = 'https://rickandmortyapi.com/graphql';
       const options = {
@@ -24,17 +41,22 @@ const Search = (props: RouteComponentProps) => {
         })
       };
       let response = await fetch(url, options);
-      const responseJSON: any = await response.json();
-      if (responseJSON.errors) {
+      const responseJSON: GraphQLResponse = await response.json();
+      if ((responseJSON as Errors).errors) {
         throw new Error('GraphQL Error');
       }
       setFetchState({
         isLoading: false,
-        data: responseJSON.data,
+        data: (responseJSON as Data).data,
         error: null
       });
     } catch (err) {
-      throw new Error('oi');
+      setFetchState({
+        isLoading: false,
+        data: null,
+        error: err.message
+      });
+      return;
     }
     return [
       {
