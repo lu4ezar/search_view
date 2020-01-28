b7287f0353a14cbaf2a79667b9468726901aa911
lu4ezar <misha-shisha@mail.ru>
2020-01-28 11:22:43 +0700

    Home header

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 
-const Home = (props: RouteComponentProps) => <>Home</>;
+const Home = (props: RouteComponentProps) => (
+  <div className="main-header">Home</div>
+);
 
 export default Home;
