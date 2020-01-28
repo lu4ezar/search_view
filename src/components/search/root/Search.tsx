1d3e87d5076278b328c37cc0b1309d6fca169f13
lu4ezar <misha-shisha@mail.ru>
2020-01-28 11:20:39 +0700

    filled Search component

--- src/components/search/root/Search.tsx
+++ src/components/search/root/Search.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import Input from '../../input';
+import Card from '../card';
+import useFetch from '../utils/useFetch';
 
-const Search = (props: RouteComponentProps) => <>Search</>;
+const Search = (props: RouteComponentProps) => {
+  const [id, setId] = useState('');
+  const [response, doFetch] = useFetch(id);
+  return (
+    <div className="container">
+      <div className="main-header">Search</div>
+      <Input
+        label="character id:"
+        value={id}
+        type="text"
+        onChange={setId}
+        onSubmit={doFetch}
+      />
+      {id && <Card {...response} />}
+    </div>
+  );
+};
 
 export default Search;
